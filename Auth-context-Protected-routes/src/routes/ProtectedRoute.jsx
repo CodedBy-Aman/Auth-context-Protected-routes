90a8f074da3e4ede9d237ab2a5bc92d8e2064ddf
Amanpreet Singh <amansg421@gmail.com>
2025-06-21 00:29:06 +0530

    used Navigate element insted of navigate hook

--- Auth-context-Protected-routes/src/routes/ProtectedRoute.jsx
+++ Auth-context-Protected-routes/src/routes/ProtectedRoute.jsx
@@ -1,11 +1,10 @@
 import React, { Children, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
-    const navigate = useNavigate();
+const ProtectedRoute =({ children }) => {
   const { isLoggedIn } = useContext(AuthContext);
-  return isLoggedIn ? children : navigate("/login" );
+  return isLoggedIn ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
