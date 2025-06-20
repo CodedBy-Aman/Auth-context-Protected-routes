b210681787940a0bdf1e3634ff6d9b5ae6caf9f8
Amanpreet Singh <amansg421@gmail.com>
2025-06-21 00:26:41 +0530

    created dashboard page

--- Auth-context-Protected-routes/src/pages/Dashboard.jsx
+++ Auth-context-Protected-routes/src/pages/Dashboard.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 
 const Dashboard = () => {
+
+    const {logout} = useContext(AuthContext)
   return (
-    <div>Dashboard</div>
+    <div className='flex flex-col gap-20 justify-center items-center '>
+        <h1 className='text-4xl font-md decoration underline text-center'>Dashboard</h1>
+        <button onClick={logout} className='px-4 py-2 bg-blue-700 hover:bg-blue-500 hover:scale-95 tracking-wide cursor-pointer'>Log-out</button>
+    </div>
   )
 }
-
+ 
 export default Dashboard
