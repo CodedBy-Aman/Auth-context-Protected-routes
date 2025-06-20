8a97c292e67ef7917f1434b312c986f265d4e6a8
Amanpreet Singh <amansg421@gmail.com>
2025-06-21 00:25:05 +0530

    added alert for not logged in

--- Auth-context-Protected-routes/src/component/Nav.jsx
+++ Auth-context-Protected-routes/src/component/Nav.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const Nav = () => {
+    const { isLoggedIn} = useContext(AuthContext)
   return (
-    <nav className="flex gap-15 px-4 py-2 bg-blue-100 rounded-xl justify-center items-center w-1/3 mx-auto mb-10">
+    <nav className="flex gap-15 px-4 py-2 bg-blue-100 rounded-xl justify-center items-center w-1/3 mx-auto mb-20">
       <NavLink to={"/"} className={({ isActive }) => isActive? "text-blue-400" : ""}>
         Home
       </NavLink>
       <NavLink to={"/login"} className={({ isActive }) => isActive? "text-blue-400" : ""}>
         Login
       </NavLink>
-      <NavLink to={"/dashboard"} className={({ isActive }) => isActive? "text-blue-400" : ""}>
+      <NavLink to={"/dashboard"} onClick={() => isLoggedIn? "" : alert("You are not logged in!")} className={({ isActive }) => isActive? "text-blue-400" : ""}>
         Dashboard
       </NavLink>
     </nav>
