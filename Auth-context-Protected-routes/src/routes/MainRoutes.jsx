8e7b64fd80265a6fc1d89090bab80f569326e635
Amanpreet Singh <amansg421@gmail.com>
2025-06-20 21:07:31 +0530

    installed packages

---
+++ Auth-context-Protected-routes/src/routes/MainRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {route, routes} from "react-router-dom"
+
+const MainRoutes = () => {
+  return (
+    <routes>
+        <route path="/" />
+    </routes>
+  )
+}
+
+export default MainRoutes
