82d3ab1de6314edf617944826ab032adc708d8e3
Amanpreet Singh <amansg421@gmail.com>
2025-06-21 00:27:02 +0530

    added welcome heading for hom epage

--- Auth-context-Protected-routes/src/pages/Home.jsx
+++ Auth-context-Protected-routes/src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='px-[10%]'>Home</div>
+    <div className='px-[10%]'>
+       <h1 className='text-2xl text-center font-md '>Welcome to the Home Page.</h1>
+    </div>
   )
 }
 
