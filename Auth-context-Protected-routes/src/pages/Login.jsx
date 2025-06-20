51f4ae4b7926c9d755635ef988860dc11f272192
Amanpreet Singh <amansg421@gmail.com>
2025-06-21 00:27:20 +0530

    crted login page

--- Auth-context-Protected-routes/src/pages/Login.jsx
+++ Auth-context-Protected-routes/src/pages/Login.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+ const {login, isLoggedIn} = useContext(AuthContext)
+  const handleLogin = () => {
+    navigate("/dashboard");
+    login();
+    
+  };
   return (
-    <div>Login</div>
+    <div className='flex flex-col gap-20 justify-center items-center '>
+        <h1 className='text-4xl font-md decoration underline text-center'>
+            Login Page
+        </h1>
+        {isLoggedIn? (<h1 className='text-2xl font-normal'>You are already logged in.</h1> ) : <button onClick={handleLogin} className='cursor-pointer px-4 py-2 bg-blue-700 hover:bg-blue-500 hover:scale-95 tracking-wide'>Log-In</button>}
+    </div>
   )
 }
 
