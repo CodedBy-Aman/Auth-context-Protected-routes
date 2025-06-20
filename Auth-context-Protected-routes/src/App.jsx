a2c85fa8b9ad6b407453d1c78b8fc134b581ce18
Amanpreet Singh <amansg421@gmail.com>
2025-06-20 23:14:19 +0530

    added nav and mainroutes

--- Auth-context-Protected-routes/src/App.jsx
+++ Auth-context-Protected-routes/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import MainRoutes from './routes/MainRoutes'
+import Nav from './component/Nav'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-screen h-screen px-[5%] py-5 bg-red-300  '>
+      
+      <Nav/>
+<MainRoutes/>
+    </div>
   )
 }
 
